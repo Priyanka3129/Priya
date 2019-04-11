086bb9ae995727941ca25b082c3235bbb00679e5
Priyanka3129 <49433132+Priyanka3129@users.noreply.github.com>
2019-04-11 10:43:14 +0530

    Add files via upload

---
+++ 9.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+?>
+<html>
+<form action="9_1.php" method="post">
+name:<input type="text" name="uname"><br>
+preference:<select name="id" >
+  <option value=""></option>
+  <option value="Python">Python</option>
+  <option value="PHP">PHP</option>
+    </select><br>
+<input type="submit" name="submit" value=submit>
+</form>
+</html>
